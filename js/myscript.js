f1faae18aa161752088a0a6728dc0657a230ba5b
yh3681 <60871983+yh3681@users.noreply.github.com>
2020-02-10 16:17:14 +0800

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,33 @@
+function doAlert(){
+    
+    alert('Hi');
+    myMessage('Hello');
+    
+    var myVariable;
+    myVariable = 'Mike';
+    mySecondMessage(myVariable);
+    
+    var myData = ['One','Two','Three'];
+    myThirdMessage(myData[0]);
+    
+}
+
+function myMessage(a){
+    
+    alert(a);
+}
+
+function mySecondMessage(a){
+    
+    alert(a);
+}
+
+function myThirdMessage(a){
+    
+    alert(a);
+}
+
+function myText(a){
+    
+    alert(a);
+}
